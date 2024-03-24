3a7d7e910539a62f7100b653619ed422663d5132
SutrisnoSukarno <37267881+sutrisnosukarno@users.noreply.github.com>
2024-03-24 16:03:45 +0700

    Add files via upload

---
+++ satu.js
@@ -0,0 +1,11 @@
+//ini adalah komentar  1 baris yang ga akan di eksekusi
+
+document.writeln ("ini adalah file js pertama saya yang ada di file satu.js");
+
+/*ini adalah
+komentar 2 baris dst
+yang tidak akan di eksekusi
+
+*/
+
+document.writeln ("ini adalah file js pertama saya yang ada di file satu.js");
